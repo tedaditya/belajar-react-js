6c2396db33755a9ddae2156a98bd5def0e5c776c
tedaditya <tedaditya@gmail.com>
2022-03-29 19:46:04 +0700

    Add hello.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Hello from './hello';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hello />
+    </>
+
+  );
+}
+
+export default App;
--- src/hello.jsx
+++ src/hello.jsx
@@ -1,3 +1,11 @@
-function hello(){
-    
+import React from 'react';
+
+function Hello(){
+    return (
+        <>
+            <h1>Hello World!</h1>
+        </>
+    );
 }
+
+export default Hello; 
